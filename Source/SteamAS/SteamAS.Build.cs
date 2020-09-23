af343877baa08eb9a99d25ae3dad1bc1bf2af04d
Aaron.rklsImmersion <awinebrenner@gmail.com>
2020-09-23 12:06:36 -0500

    Integrating Steamworks SDK
    It's in there and working correctly, now I have to try and integrate Steam Sockets

--- Source/SteamAS/SteamAS.Build.cs
+++ Source/SteamAS/SteamAS.Build.cs
@@ -6,9 +6,16 @@ public class SteamAS : ModuleRules
 {
 	public SteamAS(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        // in the video he removes this because "he doesn't want shared headers" and says you can leave it in, but he removes it because
+        // he had issues in the past with "it" randomly not finding includes anymore
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        // he replaces it with bFasterWithoutUnity = true; which, according his comment, speeds up build times by telling Unreal Build
+        // Tools to not combine Source Files
+        // https://answers.unrealengine.com/questions/3647/how-to-improve-compile-times-for-a-c-project.html?sort=oldest
+        // this post explains it a bit better, and it does sound like a good idea to have it enabled
+        bFasterWithoutUnity = true;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineSubsystemUtils", "Steamworks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
@@ -16,7 +23,7 @@ public class SteamAS : ModuleRules
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
